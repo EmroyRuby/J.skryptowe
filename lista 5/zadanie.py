40d30b7eeb5e58b3c6d42c6db6b656781cdebc3c
PK <pi.kusnierz@gmail.com>
2022-05-04 22:58:57 +0200

    __main__ == main

--- lista 5/zadanie.py
+++ lista 5/zadanie.py
@@ -1,11 +1,17 @@
 from py4j.java_gateway import JavaGateway
 
-gateway = JavaGateway()
-graph = gateway.entry_point.getGraph()
-graph.addEdge(1, 2)
-graph.addEdge(1, 3)
-graph.addEdge(1, 4)
-graph.addEdge(2, 4)
-print(graph)
-print(graph.DFS(1))
-print(graph.BFS(1))
+
+def main():
+    gateway = JavaGateway()
+    graph = gateway.entry_point.getGraph()
+    graph.addEdge(1, 2)
+    graph.addEdge(1, 3)
+    graph.addEdge(1, 4)
+    graph.addEdge(2, 4)
+    print(graph)
+    print(graph.DFS(1))
+    print(graph.BFS(1))
+
+
+if __name__ == "__main__":
+    main()
