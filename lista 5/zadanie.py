2a9d0285ecdf211f92daa3ef085743459a2e103f
PK <pi.kusnierz@gmail.com>
2022-05-05 10:06:35 +0200

    add graph2

--- lista 5/zadanie.py
+++ lista 5/zadanie.py
@@ -3,12 +3,16 @@ from py4j.java_gateway import JavaGateway
 
 def main():
     gateway = JavaGateway()
-    graph = gateway.entry_point.getGraph()
+    entry = gateway.entry_point
+    graph = entry.getGraph()
     graph.addEdge(1, 2)
     graph.addEdge(1, 3)
     graph.addEdge(1, 4)
     graph.addEdge(2, 4)
+    graph2 = entry.getGraph()
+    graph2.addEdge("wroclaw", "krakow")
     print(graph)
+    print(graph2)
     print(graph.DFS(1))
     print(graph.BFS(1))
 
